3a683cb067f7bb17b45776351702846e50243cc4
linux07source <linuxsource07@gmail.com>
2026-04-05 04:19:20 +0200

    Primo rilascio: Supporto Variadic Templates e Colori

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "term.h"
-#include <iostream>
-
-int main() {
-  term::drawbox("SWAG TERM ONLINE");
-  term::print("Caricamento moduli...", term::YELLOW);
-  term::print("Sistema pronto!", term::GREEN);
-  return 0;
-}
